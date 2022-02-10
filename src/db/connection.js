30bca61591d8b7d0f27ce7f114561c4835b0a932
DannyABoat <dannysum07@yahoo.com>
2022-02-10 19:59:09 +0000

    SQL add and read operation for CRUD operations create

---
+++ src/db/connection.js
@@ -0,0 +1,22 @@
+const {Sequelize} = require("sequelize");
+require("dotenv").config();
+
+let sequelize = new Sequelize(process.env.MYSQL_URI);
+
+// sequelize.authenticate() .then(() =>{
+//     console.log('connected to db')
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+let connection = async() =>{
+    try {
+        await sequelize.authenticate()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+connection()
+
+module.exports = sequelize
